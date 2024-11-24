b5da238ddd2e3720e893921917fba5ceabc2d990
Izundu Clara Chinecherem <ci.chinecherem@stu.unizik.edu.ng>
2024-11-24 16:38:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("OnePiece pirate login")
+username=input("Username: ")
+password=input("Password: ")
+if username=="Luffy" and password=="onepiece":
+  print("Welcome Luffy the Captain of the Straw Hat Pirates")
+elif username=="Zoro" and password=="swordsman":
+  print("Welcome Zoro the Swordsman")
+elif username=="Nami" and password=="navigator":
+  print("Welcome Nami the Navigator")
+elif username=="Usopp" and password=="firstmate":
+  print( "Welcome Usopp the First Mate of the Straw  Hat Pirates")
+elif username=="Sanji" and password=="cook":
+  print("Welcome Sanji the Cook of the Straw Hat Pirates")
+else:
+  print("You are not one of us. Do you want to join the Straw hat Pirates?")
